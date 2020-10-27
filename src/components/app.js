127a0976e26bf44809949bbf92cb7cca51d19115
JohnattanIsrael <johnattanisraelangeles@gmail.com>
2020-10-27 14:17:23 -0600

    added styles for the course library and courses list added in myschedule container

--- src/components/app.js
+++ src/components/app.js
@@ -5,9 +5,16 @@ import CourseLibrary from './courseLibrary';
 export default class App extends Component {
   render() {
     return (
-      <div className='app'>
-        <h1>DevCamp React Starter</h1>
-        <CourseLibrary />
+      <div className='main-container'>
+        <div className='library'>
+          <h1 className='library__header'>Course Library</h1>
+          <CourseLibrary />
+        </div>
+        <div className='schedule'>
+          <h1 className='schedule__header'>
+            My Schedule
+          </h1>
+        </div>
       </div>
     );
   }
